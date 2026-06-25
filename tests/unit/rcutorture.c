30e858ae2a7f209df8035a147ea9322a9694e3ae
Martin Johnson <M.J.Johnson@massey.ac.nz>
2026-06-26 11:38:00 +1200

    Fix build on windows

--- tests/unit/rcutorture.c
+++ tests/unit/rcutorture.c
@@ -240,7 +240,7 @@ int n_mberror;
 /* Updates protected by counts_mutex */
 long long rcu_stress_count[RCU_STRESS_PIPE_LEN + 1];
 
-
+volatile int garbage = 0;
 static void *rcu_read_stress_test(void *arg)
 {
     int i;
@@ -248,7 +248,6 @@ static void *rcu_read_stress_test(void *arg)
     int pc;
     long long n_reads_local = 0;
     long long rcu_stress_local[RCU_STRESS_PIPE_LEN + 1] = { 0 };
-    volatile int garbage = 0;
 
     rcu_register_thread();
 
